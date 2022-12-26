8e3f097d6f0d15aa35525ff2dbcc164b229ba460
Sirui Zhang <siruizhangdev@gmail.com>
2022-12-26 17:47:21 -0500

    add custom deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,18 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
-import * as exec from "@actions/exec";
+import * as exec from '@actions/exec';
+// import * as github from '@actions/github';
 
 const run = () => {
+    // Get inputs
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    // Upload files
+    const s3Url = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Url} --region ${bucketRegion}`);
+
+
     core.notice('Hello from my custom JavaScript action!');
 };
-run()
+run();
